5a1f45cff1d0cc0757d4acc512e63b4389654aa4
Salvador Guzman <guzmansalv@gmail.com>
2018-10-30 14:53:28 -0700

    model

--- django_youtube_app/models.py
+++ django_youtube_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class ChanStats(models.Model):
+    time = models.DateTimeField(primary_key=True)
+    serial = models.CharField(max_length=24)
+    subs = models.BigIntegerField()
+    videos = models.IntegerField()
+    views = models.BigIntegerField()
+
+    class Meta:
+        db_table = 'chan_stats'
